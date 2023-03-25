3a00aed506feb1fa1af6822797be5a472125715e
respat <respergerpatrik@gmail.com>
2023-03-25 20:24:34 +0100

    Fejlesztések, tábla generálás

--- src/app.js
+++ src/app.js
@@ -7,3 +7,55 @@
 * Github: https://github.com/respat/
 * Licenc: GNU GPL
 */
+
+let ships = [];
+
+
+window.addEventListener('load', () => {
+    init();
+    getShips();
+});
+
+function init() {
+    const tbody = document.querySelector('#tbody');
+};
+
+function getShips() {
+    let host = 'http://localhost:8000/';
+    let endpoint = 'ships'
+    let url = host + endpoint;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            ships = result;
+            render();
+        });
+
+};
+
+
+function render() {
+    ships.forEach((ship) => {
+        console.log('hello')
+        let tr = document.createElement('tr');
+        let tdName = document.createElement('td');
+        let tdLength = document.createElement('td');
+        let tdPrice = document.createElement('td');
+        let tdPerson = document.createElement('td');
+        let tdTrailer = document.createElement('td');
+        tdName.textContent = ship.name;
+        tdName.classList.add('name')
+        tdLength.textContent = ship.length;
+        tdPrice.textContent = ship.price + " HUF";
+        tdPerson.textContent = ship.person;
+        tdTrailer.textContent = ship.trailer;
+        tbody.append(tr);
+        tr.append(tdName);
+        tr.append(tdLength);
+        tr.append(tdPrice);
+        tr.append(tdPerson);
+        tr.append(tdTrailer);
+
+    });
+}
